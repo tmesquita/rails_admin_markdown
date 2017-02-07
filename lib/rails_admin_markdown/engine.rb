f794a41c6a501a478404f38bc8949190eb24b964
tmesquita <thomas.mesquita@welltok.com>
2017-02-07 16:48:37 -0500

    configuring asset precomilation

--- lib/rails_admin_markdown/engine.rb
+++ lib/rails_admin_markdown/engine.rb
@@ -1,4 +1,7 @@
 module RailsAdminMarkdown
   class Engine < ::Rails::Engine
+    initializer "RailsAdminMarkdown precompile hook", group: :all do |app|
+      app.config.assets.precompile += %w(simple_mde/simplemde.min.js simple_mde/simplemde.min.css)
+    end
   end
 end
