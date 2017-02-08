a38d4081daac53f01d0b4991039b764c86012d8e
tmesquita <thomas.mesquita@welltok.com>
2017-02-08 11:11:51 -0500

    only rendering markdown if a value is present

--- lib/rails_admin_markdown/rails_admin/config/fields/types/markdown.rb
+++ lib/rails_admin_markdown/rails_admin/config/fields/types/markdown.rb
@@ -3,11 +3,15 @@ module RailsAdmin::Config::Fields::Types
     RailsAdmin::Config::Fields::Types::register(self)
 
     register_instance_option :pretty_value do
-      html_renderer = Redcarpet::Render::HTML.new filter_html: true,
-                                                  no_images: true,
-                                                  prettify: true
+      if value.present?
+        html_renderer = Redcarpet::Render::HTML.new filter_html: true,
+                                                    no_images: true,
+                                                    prettify: true
 
-      Redcarpet::Markdown.new(html_renderer, autolink: true).render(value).html_safe
+        Redcarpet::Markdown.new(html_renderer, autolink: true).render(value).html_safe
+      else
+        ' - '
+      end
     end
 
     register_instance_option :partial do
